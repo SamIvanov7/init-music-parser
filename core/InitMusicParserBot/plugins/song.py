a9a27bf37620fc81bb315759efa62c5b28e12a06
SamIvanov7 <sam1206morfey@gmail.com>
2024-11-09 22:02:59 +0100

    update

--- core/InitMusicParserBot/plugins/song.py
+++ core/InitMusicParserBot/plugins/song.py
@@ -107,7 +107,6 @@ async def song_search(client, message):
         ],
         "keepvideo": False,
         "cookiefile": ".cookies.txt",
-        "cookiesfrombrowser": ("firefox",),
         "http_headers": {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
